e38fa6deb1e61a0d91b8426476aba194e07e03bc
MarsRaptor <waltongregory.hobby@gmail.com>
2018-07-29 14:20:16 +0200

    making sure dist is clean

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,1 +1,5 @@
-export {};
+export { Component, ComponentManager } from './component';
+export { ECSContext } from './context';
+export { Entity, EntityManager, EntityObserver } from './entity';
+export { Manager } from './manager';
+export { EntitySystem, EntitySystemManager } from './system';
